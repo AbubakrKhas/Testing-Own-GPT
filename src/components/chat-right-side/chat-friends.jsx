e364ef327cadefb2e561613aad48a4d16fd02988
Abubakr Khasanov <abubakr.khasanov@pantopix.com>
2023-12-12 14:55:17 +0100

    added gadget for more options

--- src/components/chat-right-side/chat-friends.jsx
+++ src/components/chat-right-side/chat-friends.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FaPlus, FaHome, FaUser, FaKey, FaFacebookMessenger, FaGamepad } from 'react-icons/fa';
+import { FaPlus, FaHome, FaUser, FaKey, FaFacebookMessenger, FaGamepad, FaSmoking, FaUsers, FaShare } from 'react-icons/fa';
 
 class ChatSide extends Component {
     constructor(props) {
@@ -36,9 +36,9 @@ class ChatSide extends Component {
                     <li style={{ '--i': 2 }} onClick={this.activeLink}><a ><FaFacebookMessenger /></a> </li>
                     <li style={{ '--i': 3 }} onClick={this.activeLink}><a ><FaHome /></a> </li>
                     <li style={{ '--i': 4 }} onClick={this.activeLink}><a ><FaGamepad /></a> </li>
-                    <li style={{ '--i': 5 }} onClick={this.activeLink}><a ><FaHome /></a> </li>
-                    <li style={{ '--i': 6 }} onClick={this.activeLink}><a ><FaHome /></a> </li>
-                    <li style={{ '--i': 7 }} onClick={this.activeLink}><a ><FaHome /></a> </li>
+                    <li style={{ '--i': 5 }} onClick={this.activeLink}><a ><FaShare /></a> </li>
+                    <li style={{ '--i': 6 }} onClick={this.activeLink}><a ><FaSmoking /></a> </li>
+                    <li style={{ '--i': 7 }} onClick={this.activeLink}><a ><FaUsers /></a> </li>
                     <div className="indicator"></div>
                 </ul>
             </div>
